ae0df0665d177ea8e9e05a36dbef30159420ec92
Lodisoft <marco-ccr@hotmail.com>
2021-04-08 02:29:37 -0500

    debug acivado

--- app/config/settings.py
+++ app/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ')9#bsm(u^asn8xv^)t0lzo^_^17xg%0nl4ha#1ph13jk_jqe9i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
